2b82102cb5f27dcc351cec3baf28706030127932
Kathir Sid Vel <vkathirvel@gmail.com>
2020-09-29 23:14:17 +0100

    Setup Django admin

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -31,13 +31,13 @@ class ModelTests(TestCase):
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None, 'test123')
+            get_user_model().objects.create_user(None, 'TestPass123')
 
     def test_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
-            'test@londonappdev.com',
-            'test123'
+            'vkathirvel+test@gmail.com',
+            'TestPass123'
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
